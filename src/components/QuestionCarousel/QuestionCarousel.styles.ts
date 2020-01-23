34f27c7ef94282a786a5d855c053649243b845ce
Sampo Lahtinen <lampo.sahtinen@gmail.com>
2020-01-23 20:47:14 +0100

    [update] next and back button css

--- src/components/QuestionCarousel/QuestionCarousel.styles.ts
+++ src/components/QuestionCarousel/QuestionCarousel.styles.ts
@@ -1,19 +1,27 @@
 import styled, { css } from "styled-components";
 
 import { Button } from "../Elements/Button";
+import { colors } from "../../styles/colors";
 
 export const ButtonsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-export const NextQuestionButton = styled(Button)`
+export const NextQuestionButton = styled(Button)<any>`
+  background: transparent;
+  border: none;
+  color: ${colors.lila};
+  font-size: 20px;
+  width: auto;
   ${props =>
     props.disabled &&
     css`
-      background-color: grey;
+      /* background-color: grey; */
+      color: ${colors.grey};
+      cursor: default;
       border: 0;
-    `}
+    `};
 `;
 
 export const QuestionCarouselContainer = styled.div`
