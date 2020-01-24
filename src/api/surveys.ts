6291417bbd515c3bd042203c3703566b57dec095
Sampo Lahtinen <lampo.sahtinen@gmail.com>
2020-01-24 11:27:52 +0100

    [remove] unnecessary comment

--- src/api/surveys.ts
+++ src/api/surveys.ts
@@ -5,7 +5,7 @@ export function getAllSurveys() {
   const apiUrl = `${process.env.REACT_APP_API_URL}/surveys`;
   return axios.get(apiUrl);
 }
-// TODO: Add API url to .env
+
 export function getSurveyDetails(id: string) {
   const apiUrl = `${process.env.REACT_APP_API_URL}/surveys/${id}`;
   return axios.get(apiUrl);
